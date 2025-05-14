67021a379fbad9bfdbb801e4a09ea9c9ba63dfb3
sailorjacob <x2sides@gmail.com>
2025-05-14 17:25:18 -0400

    Remove back arrow from Return Home link on game page

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -776,11 +776,8 @@ export default function GamePage() {
       <div className="mt-6">
         <Link 
           href="/" 
-          className="text-lg font-semibold text-orange-400 hover:text-orange-300 transition-colors duration-200 flex items-center"
+          className="text-lg font-semibold text-orange-400 hover:text-orange-300 transition-colors duration-200"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
-          </svg>
           Return Home
         </Link>
       </div>
